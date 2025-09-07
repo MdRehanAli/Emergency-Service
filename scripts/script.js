fc479c4c73868aaf80bbff4db0bf167b6b89c179
Md Rehan Ali <rehanali21d@gmail.com>
2025-09-08 03:12:09 +0600

    Heart Count Functionality done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-console.log("JS File Connected")
+// Heart Icon Feature 
+
+const hearts = document.getElementsByClassName("heart-button")
+let heartCount = parseInt(document.getElementById('heart').innerText);
+
+for (const heart of hearts) {
+    heart.addEventListener('click', function () {
+        heartCount++;
+        document.getElementById('heart').innerHTML = heartCount;
+    })
+}
+
